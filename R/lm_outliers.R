bd9729bf4daa77d4c26ec65c0b0eeab2ec67c5fa
Cedric Batailler <cedric.batailler@univ-grenoble-alpes.fr>
2017-09-26 10:39:02 +0200

    Initial commit.

---
+++ R/lm_outliers.R
@@ -0,0 +1,48 @@
+#' Returns a data.frame with a specific model's studentized residuals,
+#' residuals' cook's distance, and residuals' hat values.
+#'
+#' @param data A data.frame.
+#' @param formula A model formula.
+#' @param id A column name from \code{data} used to identify observations.
+#'
+#' @author Dominique Muller, \email{dominique.muller@@univ-grenoble-alpes.fr}
+#'
+#' @references Judd, C. M., McClelland, G. H., & Ryan, C. S. (2009). Data analysis: a model comparison approach (2nd ed). New York ; Hove: Routledge.
+
+#' @import dplyr
+#' @import rlang
+#' @importFrom stats model.frame
+#' @importFrom stats lm
+#' @importFrom stats hatvalues
+#' @importFrom stats cooks.distance
+#' @importFrom stats rstudent
+#'
+#' @export
+
+lm_outliers <- function(data, formula, id) {
+  # Packages check
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("dplyr needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  if (!requireNamespace("rlang", quietly = TRUE)) {
+    stop("rlang needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  # Function
+  name_id <- enquo(id)
+
+  fit <- lm(formula, data)
+  Out <- model.frame(fit)
+  Out$sdr      <- rstudent(fit)
+  Out$cookd    <- cooks.distance(fit)
+  Out$leverage <- hatvalues(fit)
+
+  data %>%
+    select(!!name_id) %>%
+    cbind(Out) %>%
+    arrange(desc(cookd)) %>%
+    print()
+}
